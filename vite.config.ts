b07b58f6a6c1359cfd94f79e74466a45bfab7338
pranesh-med <praneshtm20@gmail.com>
2026-07-19 00:50:46 -0700

    Fix plugin order in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/eleetgummyz/',
   plugins: [react()],
-  base: '/eleetgummyz/',
-  base: '/eleetgummyz/',
 });
